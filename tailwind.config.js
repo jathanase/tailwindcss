12a7f9abf9325837ed2654adb2f7646391822f69
jathanase <83649563+jathanase@users.noreply.github.com>
2022-01-23 16:38:17 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                gold: {
+                    light: '#ddbf5f',
+                    base: '#d4af37',
+                    dark: '#aa8c2c'
+                },
+            },
+        },
+
+    },
+    plugins: [],
+}
